6d2a48f925da06692c9aee27674e4257d7552bc4
sabernn <60275211+sabernn@users.noreply.github.com>
2020-02-28 16:34:54 -0600

    Adding a function for calculation of resonance amplitude

---
+++ ResonanceAmplitude.m
@@ -0,0 +1,13 @@
+function A = ResonanceAmplitude(z0,gamma,L,wn,rho,S,E,I)
+% This function gives the amplitude of vibration in resonance conditions.
+% Value of gamma should NOT be zero.
+%   Detailed explanation goes here
+if gamma==0
+    A=NaN;
+    "The amplitude of resonance for undamped system is infinity!"
+else
+    A=4*z0/gamma/L*sqrt(wn)/nthroot(rho*S/E/I);
+end
+
+end
+
