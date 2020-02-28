5772794148f9897e8537b7b7f251f6cd0848ec41
sabernn <60275211+sabernn@users.noreply.github.com>
2020-02-28 16:18:19 -0600

    defining the variables. Not done yet.

---
+++ cantilever_beam.m
@@ -0,0 +1,19 @@
+%% Title
+% Calculations of cantilever beam under vibration
+% Ref:
+% Repetto, C. & Roatta, A. & Welti, Reinaldo. (2012). Forced vibrations of a cantilever beam. European Journal of Physics - EUR J PHYS. 33. 1187-1195. 10.1088/0143-0807/33/5/1187. 
+
+%% Defining the variables
+
+clc
+clear
+
+E=200e9;        % Modullus of elasticity (Pa)
+rho=7850;       % Density (kg/m^3)
+b=3e-3;         % Beam width (m)
+h=0.508e-3;     % Beam thickness (m)
+gamma=1e-5;     % Damping coefficient (1/s)
+
+
+
+
