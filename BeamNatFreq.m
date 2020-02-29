a6fe488534ce6078c6d2cc07518294c1ff979edb
sabernn <60275211+sabernn@users.noreply.github.com>
2020-02-29 17:48:36 -0600

    the natural frequencies are now verified with Excel file

--- BeamNatFreq.m
+++ BeamNatFreq.m
@@ -3,8 +3,8 @@ function [wn1, wn2, wn3] = BeamNatFreq(h,L,E,rho)
 %   Detailed explanation goes here
 alpha=[1.875 4.694 7.885];
 
-wn1=alpha(1)*sqrt(E./12./rho).*h./L.^2;
-wn2=alpha(2)*sqrt(E./12./rho).*h./L.^2;
-wn3=alpha(3)*sqrt(E./12./rho).*h./L.^2;
+wn1=alpha(1)^2*sqrt(E./12./rho).*h./L.^2/2/pi;
+wn2=alpha(2)^2*sqrt(E./12./rho).*h./L.^2/2/pi;
+wn3=alpha(3)^2*sqrt(E./12./rho).*h./L.^2/2/pi;
 end
 
