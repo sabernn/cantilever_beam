8b830088ad5caea7b7e1d630cd46f9a4f9b43e7b
sabernn <60275211+sabernn@users.noreply.github.com>
2020-02-29 19:02:09 -0600

    Calculating max amplitude for different frequencies
    THIS IS JUST FOR THE FIRST FREQUENCY. Further verification is also required.

--- cantilever_beam.m
+++ cantilever_beam.m
@@ -14,23 +14,34 @@ b=3e-3;         % Beam width (m)
 h=0.508e-3;     % Beam thickness (m)
 S=b*h;          % Cross section (m^2)
 I=b*h^3/12;     % Second moment of inertia of cross section wrt longitudinal axis (m^4)
-L=12e-3;        % Beam length (m)
-gamma=0.01;     % Damping coefficient (1/s)
+L=4.8e-3;        % Beam length (m)
+gamma=0.0001;     % Damping coefficient (1/s)
 z0=10e-3;       % Amplitude of excitation (m)
 % w=1000:30000;         % Frequency (Hz)
+
+
+
 
 
 %% Natural Frequencies
 
 [wn1,wn2]=BeamNatFreq(h,L,E,rho)
 
-plot(L,wn2/1000,'*')
+figure
+plot(L,wn1/1000,'*')
 grid on
 
 %% Stress and Displacement Distributions
 
-% A_res=ResonanceAmplitude(z0,gamma,L,w,rho,S,E,I);
+figure
+plot(15:0.001:20,ResAmp(15000:20000,z0,gamma,L,wn1,rho,S,E,I))
+
+% Maximumm amplitude at resonance
+A_res=MaxResAmp(z0,gamma,L,wn1,rho,S,E,I);
 % 
-% plot(w,A_res)
+
+figure
+plot(wn1/1000,A_res/1000,'o')
+grid on
 
 
